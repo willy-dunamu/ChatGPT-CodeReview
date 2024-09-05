ce07afba3f86f4bfae7eeb8bb2e6eaa541f23ade
willy <willy@dunamu.com>
2024-09-05 15:02:46 +0900

    init

--- src/chat.ts
+++ src/chat.ts
@@ -8,7 +8,7 @@ export class Chat {
       apiBaseUrl:
         process.env.OPENAI_API_ENDPOINT || 'https://api.openai.com/v1',
       completionParams: {
-        model: process.env.MODEL || 'gpt-3.5-turbo',
+        model: process.env.MODEL || 'gpt-4o',
         temperature: +(process.env.temperature || 0) || 1,
         top_p: +(process.env.top_p || 0) || 1,
         max_tokens: process.env.max_tokens
